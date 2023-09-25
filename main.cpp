59cbca32c2a3550494f6d3419a21284118591f71
bellawoliver <bellawoliver@gmail.com>
2023-09-24 19:56:14 -0500

    init classes and objects

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include "library.cpp"
+// #include "book.cpp"
+
 
 using namespace std;
 
-const std::string password = "password";
+const string password = "password";
 
 int main() {
-  std::unordered_map<std::string, int> libMap;
+    Library library;
 
-  libMap["book1"] = 1;
-  libMap["book2"] = 10;
-  libMap["book3"] = 3;
-  libMap["book4"] = 5;
-  libMap["book5"] = 9;
+    Book book1("Book 1", "Author 1");
+    Book book2("Book 2", "Author 2");
+    Book book3("Book 3", "Author 3");
 
-  
-  
-  
-  return 0;
+    library.addBook(book1);
+    library.addBook(book2);
+    library.addBook(book3);
+
+    library.displayBooks();
+
+    cout << "Checking availability of Book with ID 1234: " << (library.checkAvailability("1234") ? "Yes" : "No") << endl;
+
+    library.deleteBook("2345");
+
+    cout << "After deleting Book with ID 2345:" << endl;
+    library.displayBooks();
+
+    Book updatedBook("Updated Book 1", "Updated Author 1");
+    library.updateBook("1234", updatedBook);
+
+    cout << "After updating Book with ID 1234:" << endl;
+    library.displayBooks();
+
+    return 0;
 }
-
