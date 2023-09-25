6f78e2e7ab83300040e67cfde6c41c7d161dd1b8
9TalesGithub <112519255+9TalesGithub@users.noreply.github.com>
2023-09-24 23:17:44 -0500

    Add admin password prompt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include "Library.h"
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    //Admin password protection
+    string adminFeaturesInput;
+    string passwordInput;
+    bool adminFeatures = false;
+
+    cout << "Are you using admin features? Type \"Y\" for yes." << endl;
+    cin >> adminFeaturesInput;
+
+    if (adminFeaturesInput == "Y") { //yes to admin features
+        cout << "Please type in the password:" << endl;
+        cin >> passwordInput;
+
+        if (passwordInput == "password") { //password correct
+            cout << "Password accepted. Admin features enabled." << endl;
+            adminFeatures = true;
+        }
+        else { //password incorrect
+            cout << "Password incorrect. Admin features will not be available." << endl;
+        }
+    }
+    else { //no to admin features
+        cout << "Input interpretted as \"No\". No admin features will be available." << endl;
+    }
+    
     Library library;
 
     Book book1("Book A", "Author X");
